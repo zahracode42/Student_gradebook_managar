5a2d9bc103ee034df5f65e780a27bb4fc785599a
zahracode42 <zahra.moh740@gmail.com>
2026-07-16 01:03:10 +0330

    Add add-assessment and display_info methods
    Added add_assessment() and display_info() methods to Course Class. find_assessment() depends on the Assessment Class, so I left a TODO for that.

--- course.py
+++ course.py
@@ -1,13 +1,26 @@
 class Course:
-    def __init__(self,course_code,course_name):
+    def __init__(self, course_code, course_name):
         self.course_code = course_code
         self.course_name = course_name
         self.students = []
         self.assessments = []
 
 
-    def add_student(self,student_id):
+    def add_student(self, student_id):
         if student_id not in self.students:
             self.students.append(student_id)
         else:
             print("Student is already enrolled.")
+
+    def add_assessment(self, assessment):
+        self.assessments.append(assessment)
+        # This part will be completed after creating class "Assessment"
+
+    #TODO: Create find_assessment() after creating the Assessment Class
+
+
+    def display_info(self):
+        print(f"Course Code: {self.course_code}")
+        print(f"Course Name: {self.course_name}")
+        print(f"Enrolled Students: {len(self.students)}")
+        # This part will be completed after creating class "Assessment"
