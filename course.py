f0b6da746c260a39763c73bcd12b5722832d264a
zahracode42 <zahra.moh740@gmail.com>
2026-07-15 19:50:43 +0330

    Add Course constructor
    Added another class (Course Class) and intialized its basic attributes

---
+++ course.py
@@ -0,0 +1,6 @@
+class Course:
+    def __init__(self,course_code,course_name):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.students = []
+        self.assessments = []
