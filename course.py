c98311674f8c530e4be2fd8b1b0752366916218d
zahracode42 <zahra.moh740@gmail.com>
2026-07-15 20:16:28 +0330

    Add add_student method
    Added add_student method to enroll students in a course and check for duplicate student IDs.

--- course.py
+++ course.py
@@ -4,3 +4,10 @@ class Course:
         self.course_name = course_name
         self.students = []
         self.assessments = []
+
+
+    def add_student(self,student_id):
+        if student_id not in self.students:
+            self.students.append(student_id)
+        else:
+            print("Student is already enrolled.")
