13a99396de4d98a33c3429013024dfc593ff315c
zahracode42 <zahra.moh740@gmail.com>
2026-07-15 19:26:59 +0330

    Complete Student class
    In this step, I completed student class, which represent a student in the system.
    Using OOP attributes and methods.

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from student import Student
 print("==== Student Gradebook Managar ====")
+#input("1. Add Students\n2. View Students\n3. Add course \n4. Enroll Students in Course\n")
+student1 = Student("001" , "Ahmad Rahimi", "ahmad@gmail.com")
+student2 = Student("002", "Sara Rad", "rad23@gmail.com")
---
+++ student.py
@@ -0,0 +1,37 @@
+
+class Student:
+    def __init__(self, student_id, name, email):
+        self.__student_id = student_id
+        self.__name = name
+        self.__email = email
+        self.courses = []
+
+    def get_id(self):
+        return self.__student_id
+    def get_name(self):
+        return self.__name
+
+
+    @property
+    def email(self):
+        return self.__email
+    @email.setter
+    def email(self, email):
+        if "@" in email:
+            self.__email = email
+            print("Email updated:)")
+        else:
+            print("Invalid email!")
+
+    def enroll_course(self, course_code):
+        self.courses.append(course_code)
+
+
+    def display_info(self):
+        print(f"Student ID: {self.__student_id}")
+        print(f"Name: {self.__name}")
+        print(f"Email: {self.__email}")
+        print(f"Enrolled courses: {len(self.courses)}")
+        #this part will complete after creating class "Course"
+
+
