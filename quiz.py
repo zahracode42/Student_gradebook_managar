5676756f68f424389a7e421260b8324a3d00492f
zahracode42 <zahra.moh740@gmail.com>
2026-07-16 13:11:39 +0330

    Create Quiz class
    Created the Quiz class by inheriting from Assessment. Also overrode display_info() and grade_message().

---
+++ quiz.py
@@ -0,0 +1,15 @@
+from assessment import Assessment
+
+class Quiz(Assessment):
+    def __init__(self, title, max_score):
+        super().__init__(title, max_score)
+
+    def display_info(self):
+        print(f"Quiz: {self.title} - Max score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 50:
+            return "Great quiz result!"
+        else:
+            return "Needs more practice."
