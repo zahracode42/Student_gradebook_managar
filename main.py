6ce6015d6e4057c0523f9068795fa23602f8a238
zahracode42 <zahra.moh740@gmail.com>
2026-07-18 12:53:36 +0330

    Create main menu
    Imported classes in main.py and created the main menu.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from assessment import Assessment
 from student import Student
-print("==== Student Gradebook Managar ====")
-#input("1. Add Students\n2. View Students\n3. Add course \n4. Enroll Students in Course\n")
-student1 = Student("001" , "Ahmad Rahimi", "ahmad@gmail.com")
-student2 = Student("002", "Sara Rad", "rad23@gmail.com")
+from course import Course
+from quiz import Quiz
+from exam import Exam
+from gradebook import GradeBook
+
+gradebook = GradeBook()
+
+print("==== *Student Gradebook Manager* ====")
+print("1. Add Student")
+print("2. View Students")
+print("3. Add Course")
+print("4. Enroll Student in Course")
+print("5. Add Assessment")
+print("6. Record Grade")
+print("7. View Student Report")
+print("8. Search Student")
+print("9. Delete Student")
+print("0. Exit")
